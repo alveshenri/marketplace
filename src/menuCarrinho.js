6c95c43777d528f564cd8a9bee3887437ed5569b
Henrique Alves <henryk_sa@outlook.com>
2023-09-01 11:15:05 -0300

    new feature: add product in cart

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -1,39 +1,44 @@
-function abrirCarrinho(params) {
-    document.getElementById("carrinho").classList.add('right-[0px]')
-   document.getElementById("carrinho").classList.remove('right-[-360px]')
-};
+import { catalogo } from "./utilidades";
+
+function abrirCarrinho() {
+  document.getElementById("carrinho").classList.add("right-[0px]");
+  document.getElementById("carrinho").classList.remove("right-[-360px]");
+}
 
 function fecharCarrinho() {
-   document.getElementById("carrinho").classList.remove('right-[0px]')
-   document.getElementById("carrinho").classList.add('right-[-360px]')
-};
+  document.getElementById("carrinho").classList.remove("right-[0px]");
+  document.getElementById("carrinho").classList.add("right-[-360px]");
+}
 
 export function inicializarCarrinho() {
-    const botaoFecharCarrinho = document.getElementById("fechar-carrinho")
-    const botaoAbrirCarrinho = document.getElementById("abrir-carrinho")
-    
-    botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
-    botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
-};
+  const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+  const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
 
- export function adicionarAoCarrinho() {
-    const containerProdutosCarrinho = document.getElementById('prddutos-carrinho')
-    const cartãoProdutoCarrinho =     ` <article class="flex bg-slate-100 rounded-lg p-1 relative">
+  botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
+  botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
+
+export function adicionarAoCarrinho(idProduto) {
+  const produto = catalogo.find((p) => p.id === idProduto);
+  const containerProdutosCarrinho =
+    document.getElementById("produtos-carrinho");
+  const cartaoProdutoCarrinho = ` <article class="flex bg-slate-100 rounded-lg p-1 relative">
     <button id="fechar-carrinho" class="absolute top-0 right-2">
-      <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+      <i 
+      class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"
+      ></i>
     </button>
     <img 
-    src="./assets/img/product-1.jpg" 
-    alt="Carrinho: Camisa Larga com Bolsos" 
+    src="./assets/img/${produto.imagem}" 
+    alt="Carrinho: ${produto.nome}" 
     class="h-24 rounded-lg">
 
     <div class="py-2">
-      <p class="text-slate-900 text-sm">Camisa Larga com Bolsos</p>
+      <p class="text-slate-900 text-sm">${produto.nome}</p>
       <p class="text-slate-400 text-xs">Tamanho: M</p>
-      <p class="text-green-700 text-lg">$70</p>
+      <p class="text-green-700 text-lg">$${produto.preco}</p>
     </div>
   </article>`;
-  containerProdutosCarrinho.innerHTML += cartãoProdutoCarrinho
+
+  containerProdutosCarrinho.innerHTML += cartaoProdutoCarrinho;
 }
-
-
